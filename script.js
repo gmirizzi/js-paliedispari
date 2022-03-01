568b7edda0087517d45a1ed4b490a5f63b306303
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-01 17:52:00 +0100

    function palindroma

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* document.querySelector('input:last-of-type').addEventListener('click', function () {
-    const word = document.getElementById("word").value.toLowerCase();
+//FUNCTIONS
+function isPalindroma(word){
     let check = true;
     let i = 0;
     let j = word.length - 1;
@@ -10,32 +10,13 @@
         i++;
         j--;
     }
-
+    
     if (check == false) {
-        document.querySelector('div').innerHTML = "La parola non è palindroma"
+        return false;
     } else {
-        document.querySelector('div').innerHTML = "La parola è palindroma"
+        return true;
     }
 }
-) */
-
-//PARI E DISPARI
-document.getElementById('btn').addEventListener("click", function () {
-    const userNum = parseInt(document.getElementById('num').value);
-    
-    if (userNum < 1 || userNum > 5) {
-        alert('Devi inserire un numero tra 1 e 5!');
-    }
-    const cpuNum = randomNumber(1, 5);
-    document.getElementById('cpu').innerHTML = cpuNum;
-    const somma = userNum + cpuNum;
-    console.log(somma, document.querySelector('select').value )
-    if ((isEven(somma) && document.querySelector('select').value == 'pari') || (!isEven(somma) && document.querySelector('select').value == 'dispari')) {
-        document.getElementById('risultato').innerHTML = 'YOU WIN!';
-    } else {
-        document.getElementById('risultato').innerHTML = 'CPU wins!';
-    }
-})
 
 function randomNumber(min, max) {
     return parseInt(Math.random() * (max - min) + min);
@@ -48,5 +29,35 @@ function isEven(num) {
         return false;
     }
 }
+
+//PALINDROMA
+document.querySelector('#palindroma input:last-of-type').addEventListener('click', function () {
+    const word = document.getElementById("word").value.toLowerCase();
+    
+    if (isPalindroma(word)) {
+        document.querySelector('#palindroma div').innerHTML = "La parola è palindroma"
+    } else {
+        document.querySelector('#palindroma div').innerHTML = "La parola non è palindroma"
+    }
+}
+)
+
+//PARI E DISPARI
+document.getElementById('btn').addEventListener("click", function () {
+    const userNum = parseInt(document.getElementById('num').value);
+    if (userNum < 1 || userNum > 5) {        
+        alert('Devi inserire un numero tra 1 e 5!');
+    } else {
+        const cpuNum = randomNumber(1, 5);
+        document.getElementById('cpu').innerHTML = "CPU butta "+cpuNum;
+        const somma = userNum + cpuNum;
+        if ((isEven(somma) && document.querySelector('select').value == 'pari') || (!isEven(somma) && document.querySelector('select').value == 'dispari')) {
+            document.getElementById('risultato').innerHTML = 'YOU WIN!';
+        } else {
+            document.getElementById('risultato').innerHTML = 'CPU wins :(';
+        }
+    }
+})
+
 
 

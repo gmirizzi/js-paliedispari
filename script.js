3f861364f1df01ce4d82518291db67d2ca0e4bb3
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-01 17:12:15 +0100

    pari e dispari

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('input:last-of-type').addEventListener('click', function () {
+/* document.querySelector('input:last-of-type').addEventListener('click', function () {
     const word = document.getElementById("word").value.toLowerCase();
     let check = true;
     let i = 0;
@@ -17,4 +17,36 @@ document.querySelector('input:last-of-type').addEventListener('click', function 
         document.querySelector('div').innerHTML = "La parola è palindroma"
     }
 }
-)
+) */
+
+//PARI E DISPARI
+document.getElementById('btn').addEventListener("click", function () {
+    const userNum = parseInt(document.getElementById('num').value);
+    
+    if (userNum < 1 || userNum > 5) {
+        alert('Devi inserire un numero tra 1 e 5!');
+    }
+    const cpuNum = randomNumber(1, 5);
+    document.getElementById('cpu').innerHTML = cpuNum;
+    const somma = userNum + cpuNum;
+    console.log(somma, document.querySelector('select').value )
+    if ((isEven(somma) && document.querySelector('select').value == 'pari') || (!isEven(somma) && document.querySelector('select').value == 'dispari')) {
+        document.getElementById('risultato').innerHTML = 'YOU WIN!';
+    } else {
+        document.getElementById('risultato').innerHTML = 'CPU wins!';
+    }
+})
+
+function randomNumber(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+}
+
+function isEven(num) {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+

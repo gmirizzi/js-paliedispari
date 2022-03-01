4803a428b2e63bb1d495c5280864ed6aa4a98dbe
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-01 15:24:20 +0100

    palindroma senza funzione

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-document.querySelector('input:last-of-type').addEventListener('click', function(){
-    const word = document.getElementById("word").value;
-    
-    if (palindroma(word,word.length)){
-        document.querySelector('div').innerHTML="La parola è palindroma"
+document.querySelector('input:last-of-type').addEventListener('click', function () {
+    const word = document.getElementById("word").value.toLowerCase();
+    let check = true;
+    let i = 0;
+    let j = word.length - 1;
+    while (i < word.length && j >= 0) {
+        if (word.charAt(i) != word.charAt(j)) {
+            check = false;
+        }
+        i++;
+        j--;
+    }
+
+    if (check == false) {
+        document.querySelector('div').innerHTML = "La parola non è palindroma"
     } else {
-        document.querySelector('div').innerHTML="La parola non è palindroma"
+        document.querySelector('div').innerHTML = "La parola è palindroma"
     }
 }
 )
-
-function palindroma(string, length){
-    for (let i = 0; i < length; i++) {
-        for (let j = length - 1; j >= 0; j--) {
-            if (string.charAt(i)==string.charAt(j)) {
-                return true;
-            } else {
-                return false;
-            }                   
-        }       
-    }
-}
